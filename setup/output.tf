c478bac2d8e14f492712ef6adadc038d0a069f44
Emerson Valentim <emerson.santos@smarkio.com.br>
2021-08-08 14:41:37 -0300

    style: add correct indent

--- setup/output.tf
+++ setup/output.tf
@@ -1,8 +1,8 @@
 output "config" {
   value = {
-    lb_address = module.load_balancer.config.dns_name
-    service = module.ecs.config.service
-    cluster = module.ecs.config.cluster
+    lb_address      = module.load_balancer.config.dns_name
+    service         = module.ecs.config.service
+    cluster         = module.ecs.config.cluster
     task-definition = module.ecs.config.task-definition
   }
 }
