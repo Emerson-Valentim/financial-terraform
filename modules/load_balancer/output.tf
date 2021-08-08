8d5a86fda0ede6a8f6ce94bc10e9e7ad54e5d137
Emerson Valentim <emerson.santos@smarkio.com.br>
2021-08-08 09:44:38 -0300

    fix: add value on loadbalancer output

--- modules/load_balancer/output.tf
+++ modules/load_balancer/output.tf
@@ -1,6 +1,8 @@
 output "config" {
-  dns_name     = aws_lb.load_balancer.dns_name
-  zone_id      = aws_lb.load_balancer.zone_id
-  target_group = aws_lb_target_group.lb_target_group.arn
-  description  = "Load balancer configs"
+  value = {
+    dns_name     = aws_lb.load_balancer.dns_name
+    zone_id      = aws_lb.load_balancer.zone_id
+    target_group = aws_lb_target_group.lb_target_group.arn
+  }
+  description = "Load balancer configs"
 }
