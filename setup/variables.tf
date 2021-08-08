2728517e792fbbebb64a3fa494f462b20f342854
Emerson Valentim <emerson.santos@smarkio.com.br>
2021-08-08 10:51:03 -0300

    fix: fix security group ids

--- setup/variables.tf
+++ setup/variables.tf
@@ -21,9 +21,9 @@ variable "network" {
   }) })
   default = {
     security_groups = {
-      load_balancer = ["subnet-0c7229dea2fed41e3"]
-      ecs           = ["subnet-0c7229dea2fed41e3"]
-      database      = ["subnet-0c7229dea2fed41e3"]
+      load_balancer = ["sg-0780e32f654d0b611"]
+      ecs           = ["sg-0780e32f654d0b611"]
+      database      = ["sg-0780e32f654d0b611"]
     }
     subnets = ["subnet-0c7229dea2fed41e3", "subnet-0a577f7f95c156171", "subnet-0ddb7f6b908336c2a"]
     vpc     = "vpc-03cbe365d724107ef"
