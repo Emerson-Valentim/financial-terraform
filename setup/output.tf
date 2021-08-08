12b326669d602b976cfe79107d82d09cbb21f378
Emerson Valentim <emerson.santos@smarkio.com.br>
2021-08-08 12:22:49 -0300

    fix: add d on lb_address variable name

--- setup/output.tf
+++ setup/output.tf
@@ -1,6 +1,6 @@
 output "config" {
   value = {
-    lb_adress = module.load_balancer.config.dns_name
+    lb_address = module.load_balancer.config.dns_name
     service = module.ecs.config.service
     cluster = module.ecs.config.cluster
     task-definition = module.ecs.config.task-definition
